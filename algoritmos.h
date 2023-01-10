f1548745ef7eb9fe0ed39fa036959e925bd7a7c9
ITM Developers <itm.developers.mx@gmail.com>
2023-01-10 11:51:02 -0600

    Base Code

---
+++ algoritmos.h
@@ -0,0 +1,12 @@
+#ifndef ALGORITMOS_H
+#define ALGORITMOS_H
+
+#include "libAlgoritmos_global.h"
+
+class LIBALGORITMOS_EXPORT Algoritmos
+{
+public:
+    Algoritmos();
+};
+
+#endif // ALGORITMOS_H
